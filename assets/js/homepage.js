3e33d9e1bded7c5a5d25e39c499e0d72292ea21e
Kaitlyn Skinner <kskinnerr@hotmail.com>
2022-02-16 12:22:37 -0500

    users can search for featured repos

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -12,6 +12,7 @@ var userFormEl= document.querySelector("#user-form");
 var nameInputEl = document.querySelector("#username");
 var repoContainerEl = document.querySelector("#repos-container");
 var repoSearchTerm = document.querySelector("#repos-search-term");
+var languageButtonsEl = document.querySelector("#language-buttons");
 
 // Handle the form submission from user's
 var formSubmitHandler = function(event) {
@@ -105,5 +106,39 @@ var displayRepos = function(repos, searchTerm) {
     }
 };
 
+// get featured repos - accepting language as the parameter
+var getFeaturedRepos = function(language) {
+    var apiUrl = "https://api.github.com/search/repositories?q=" + language + "+is:featured&sort=help-wanted-issues";
+
+    fetch(apiUrl).then(function(response) {
+        if(response.ok) {
+            console.log(response);
+            response.json().then(function(data) {
+                //console.log(data);
+                displayRepos(data.items, language);
+            });
+        } else {
+            alert('Error: GitHub User Not Found');
+        }
+    });
+};
+
+// Languages buttonClickHandler function
+var buttonClickHandler = function(event) {
+    event.preventDefault();
+
+    var language = event.target.getAttribute("data-language");
+
+    console.log(language);
+
+    if (language) {
+        getFeaturedRepos(language);
+
+        //clear old content
+        repoContainerEl.textContent = "";
+    }
+};
+
 // add event listeners to forms
 userFormEl.addEventListener("submit", formSubmitHandler);
+languageButtonsEl.addEventListener("click", buttonClickHandler);
